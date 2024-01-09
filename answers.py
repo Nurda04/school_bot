22110d08c317c659bd91b128f65b7cf76f77b0f9
Nurda04 <155241713+Nurda04@users.noreply.github.com>
2024-01-10 00:05:18 +0600

    Update answers.py

--- answers.py
+++ answers.py
@@ -3,9 +3,38 @@ import database as db
 import keyboards as kb
 
 
-def schedule_get(day, day_name, operation, time, record)->(str):
+def class_info(class_name, operation):
+    info, info2, info3, info4 = db.get_class_info(class_name)
+    members = 'Нет' if not info2 else str(len(info2))
+    admins = 'Нет' if not info3[0] else info3[0]
+    moders = 'Нет' if not info4[0] else info4[0]
+    text = f'Название: {info[0]}\nПароль: {info[1]}\n'
+    if operation == 'info':
+        text += f'Владелец: {db.get_class_creator(class_name)}\n'
+    text += f'Число участников: {members}\n'
+    text += f'Админы: {admins}\n'
+    text += f'Модераторы: {moders}'
+    return text
+
+
+def profile_get(user_id):
+    text = f'Имя: {db.get_name(user_id)}'
+    texts = {
+        3 : "\nСозданные классы:",
+        2 : "\nАдмин в:",
+        1 : "\nМодератор в:",
+        0 : "\nУчастник в:"
+    }
+    for i in reversed(range(4)):
+        profile = db.get_classes_admin(user_id, i)
+        admin = ' ' if not profile[0] else profile[0]
+        text += f'{texts.get(i)} {admin}'
+    return text
+
+
+def schedule_get(day, day_name, operation, time, record, cur_class)->(str):
     answer = f'<b>{day_name}:</b>\n'
-    schedules = db.get_schedule(day)
+    schedules = db.get_schedule(day, cur_class)
     if not schedules:
         answer += 'Нет записей предметов\n'
     else:
@@ -16,7 +45,7 @@ def schedule_get(day, day_name, operation, time, record)->(str):
                 if record == 'rec_week1':
                     a = str(schedule[4])
                     i = 1
-                else:
+                else: 
                     a = str(schedule[5])
                     i = 2
                 if a == b:
@@ -32,12 +61,12 @@ def schedule_get(day, day_name, operation, time, record)->(str):
     return answer
 
 
-def schedule_get_all(operation, week, time, record)->(str):
+def schedule_get_all(operation, week, time, record, cur_class)->(str):
     text = []
     day = ['day1', 'day2', 'day3', 'day4', 'day5']
-    day_name = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']
-    for i in range(0, 5):
-        s = schedule_get(day[i], day_name[i], operation, time, record)
+    day_name = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']    
+    for i in range(5):
+        s = schedule_get(day[i], day_name[i], operation, time, record, cur_class)
         text.append(s)
     text_all = '\n'.join(text)
     if operation == 'get_hw':
@@ -47,21 +76,20 @@ def schedule_get_all(operation, week, time, record)->(str):
         answer = f'{period[0]} - {period[1]}\n'
         answer += text_all
     else: answer = text_all
-    return answer
+    return answer                 
 
 
-def schedule_choose_obj(day, day_name)->(str, InlineKeyboardMarkup):
-    schedules = db.get_schedule(day)
+def schedule_choose_obj(day, day_name, cur_class)->(str, InlineKeyboardMarkup):
+    schedules = db.get_schedule(day, cur_class)
     answer = f'<b>{day_name}</b>'
     if not schedules:
         answer += '\nНет записей предметов'
         markup = None
-    else:
-        kb.kb_choose_obj(day)
+    else: 
         answer += ', выберите предмет:'
-        markup = kb.choose_obj
+        markup = kb.kb_choose_obj(day)
     return answer, markup
-
+        
 
 def schedule_del_hw(task_s)->(str, InlineKeyboardMarkup):
     markup = kb.kb_del_hw(len(task_s))
